e9e5c534cd6224b39a6e43cf2b6af0e82593fe49
chenyinchao <chenyinchao@foxmail.com>
2019-11-27 16:13:08 +0800

    适配android 8.x以上版本

--- app/src/main/java/com/ycchen/poetry/PoetryWidgetService.java
+++ app/src/main/java/com/ycchen/poetry/PoetryWidgetService.java
@@ -64,6 +64,8 @@ public class PoetryWidgetService extends Service {
         chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         assert manager != null;
+        manager.createNotificationChannel(chan);
+
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
         Notification notification = notificationBuilder.setOngoing(true)
                 .setSmallIcon(R.drawable.preview)
